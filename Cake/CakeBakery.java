1c2970dad8427a8fe40082ad3fe1d2632a230b24
newco <newco@192.168.1.6>
2025-12-02 17:12:14 +0300

    new update

--- Cake/CakeBakery.java
+++ Cake/CakeBakery.java
@@ -1,8 +1,22 @@
 
-public class CakeBakery {
+import javax.swing.*;
+import java.awt.*;
+public class CakeBakery extends JFrame{
+	private JTextField text1,text2,text3,text4;
+	private JLabel label1,label2;
+	private JButton button1,button2,button3,button4,button5,button6;
+	private JPanel panel1,panel2,panel3,panel4,panel5,panel6;
+	
+	public CakeBakery()
+	{
+		super("Cake Bakery");
+		setLayout(new GridLayout(6,1));
+		
+		//--------panel
+	}
+
 public static void main(String []args)
 {
-	System.out.println("h");
-	System.out.println();
+	
 }
 }
